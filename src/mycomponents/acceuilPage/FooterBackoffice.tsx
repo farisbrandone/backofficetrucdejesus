b12a3273d6bf860eea3d6f9ac3f5edf676dc1835
farisbrandone <farisbrandone@yahoo.com>
2024-10-31 08:04:57 +0100

    ee

--- src/mycomponents/acceuilPage/FooterBackoffice.tsx
+++ src/mycomponents/acceuilPage/FooterBackoffice.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 function FooterBackoffice() {
   return (
-    <div className="footerAcceuilPage absolute bottom-2 left-2">
+    <div className="footerAcceuilPage absolute bottom-2 -left-[230px] text-white">
       {format(Date.now(), "yyyy")} ©{" "}
       <strong className="text-[#fff700] ">Truc de JESUS !</strong>
     </div>
