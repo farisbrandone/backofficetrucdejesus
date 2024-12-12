7631eec039787452053ef857d2dee4b26062a914
farisbrandone <farisbrandone@yahoo.com>
2024-12-12 21:28:33 +0100

    dd

--- src/Sign/login/LoginMother.tsx
+++ src/Sign/login/LoginMother.tsx
@@ -1,11 +1,12 @@
 import { context } from "@/App";
 import { ReactNode, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginMother({ children }: { children: ReactNode }) {
   const data = useContext(context);
-
+  const navigate = useNavigate();
   if (!data) {
-    window.location.replace("/login");
+    navigate("/login");
   }
 
   return <>{data && children}</>;
